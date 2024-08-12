66f4c2d65e5c4935069989b74938d42990a7dd17
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-08-12 00:01:12 -0500

    Optimized ChunkView initialization

--- src/main/java/org/useless/seedviewer/gui/ChunkView.java
+++ src/main/java/org/useless/seedviewer/gui/ChunkView.java
@@ -20,8 +20,10 @@ public class ChunkView {
     private final int RESOLUTION_SCALE = 1;
     private final BufferedImage biomeMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_INT_ARGB);
     private final BufferedImage terrainMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_INT_ARGB);
-    private final BufferedImage heightMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_BYTE_GRAY);
-    private final BufferedImage waterDepthMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_BYTE_GRAY);
+    private final BufferedImage heightMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_INT_ARGB);
+    private final BufferedImage waterDepthMapImage = new BufferedImage(Chunk.CHUNK_SIZE_X * RESOLUTION_SCALE, Chunk.CHUNK_SIZE_Z * RESOLUTION_SCALE, BufferedImage.TYPE_INT_ARGB);
+
+    private final Rectangle bounds;
     private final ChunkLocation location;
     private final ChunkProvider provider;
 
@@ -36,47 +38,36 @@ public class ChunkView {
     public ChunkView(ChunkLocation location, ChunkProvider provider) {
         this.location = location;
         this.provider = provider;
+        this.bounds = new Rectangle(location.x * Chunk.CHUNK_SIZE_X, location.z * Chunk.CHUNK_SIZE_Z, Chunk.CHUNK_SIZE_X, Chunk.CHUNK_SIZE_Z);
 
         loadTask = InitializerService.submit(
             () -> {
                 try {
                     Chunk chunk = provider.getChunk(location);
 
-                    Graphics terrain = terrainMapImage.getGraphics();
-                    Graphics biome = biomeMapImage.getGraphics();
-                    Graphics height = heightMapImage.getGraphics();
-                    Graphics depth = waterDepthMapImage.getGraphics();
-                    terrain.setColor(new Color(1, 1, 1, 0));
-                    terrain.fillRect(0, 0, Chunk.CHUNK_SIZE_X, Chunk.CHUNK_SIZE_Z);
-                    biome.setColor(new Color(1, 1, 1, 0));
-                    biome.fillRect(0, 0, Chunk.CHUNK_SIZE_X, Chunk.CHUNK_SIZE_Z);
                     for (int x = 0; x < Chunk.CHUNK_SIZE_X; x++) {
                         for (int z = 0; z < Chunk.CHUNK_SIZE_Z; z++) {
                             ChunkPos2D pos2D = new ChunkPos2D(x, z);
                             int h = chunk.getHeight(pos2D) - 1;
                             if (h < 0) h = 0;
-                            height.setColor(new Color(h, h, h));
-                            height.fillRect(x * RESOLUTION_SCALE, z * RESOLUTION_SCALE, RESOLUTION_SCALE, RESOLUTION_SCALE);
+                            heightMapImage.setRGB(x, z, h & 0xFF);
 
-                            int wd = chunk.getWaterDepth(pos2D);
-                            depth.setColor(new Color(wd, wd, wd, 255));
-                            depth.fillRect(x * RESOLUTION_SCALE, z * RESOLUTION_SCALE, RESOLUTION_SCALE, RESOLUTION_SCALE);
+                            int wd = chunk.getWaterDepth(pos2D) & 0xFF;
+                            waterDepthMapImage.setRGB(x, z, 0xff000000 | wd << 16 | wd << 8 | wd);
 
                             Biome b = chunk.getBiome(new ChunkPos3D(x, h, z));
-                            Color biomeColor = new Color(b.getColor());
-                            biomeColor = new Color(biomeColor.getRed(), biomeColor.getGreen(), biomeColor.getBlue(), 128);
-                            biome.setColor(biomeColor);
-                            biome.fillRect(x * RESOLUTION_SCALE, z * RESOLUTION_SCALE, RESOLUTION_SCALE, RESOLUTION_SCALE);
+                            Color bc = new Color(b.getColor());
+                            bc = new Color(bc.getRed(), bc.getGreen(), bc.getBlue(), 128);
+                            biomeMapImage.setRGB(x, z, bc.getRGB());
 
                             Color c = chunk.getBlockColor(new ChunkPos3D(x, h, z));
-                            terrain.setColor(c);
-                            terrain.fillRect(x * RESOLUTION_SCALE, z * RESOLUTION_SCALE, RESOLUTION_SCALE, RESOLUTION_SCALE);
+                            if (c != null) {
+                                terrainMapImage.setRGB(x, z, c.getRGB());
+                            } else {
+                                terrainMapImage.setRGB(x, z, 0);
+                            }
                         }
                     }
-                    terrain.dispose();
-                    biome.dispose();
-                    height.dispose();
-                    depth.dispose();
                 } catch (Exception e) {
                     Global.LOGGER.error("Error when initializing chunk {}, {}", location.x, location.z, e);
                 }
@@ -102,7 +93,7 @@ public class ChunkView {
                 try {
                     for (int x = 0; x < Chunk.CHUNK_SIZE_X; x++) {
                         for (int z = 0; z < Chunk.CHUNK_SIZE_Z; z++) {
-                            int depth = waterDepthMapImage.getRGB(x, z) >> 8 & 0xFF;
+                            int depth = waterDepthMapImage.getRGB(x, z) & 0xFF;
 
                             int shade = 220;
                             if (depth == 0) {
@@ -122,7 +113,7 @@ public class ChunkView {
                                     shade = 255;
                                 }
                             } else {
-                                double d3 = (double) depth * 0.02D + (double) ((x + z) & 0b1) * 0.2D;
+                                double d3 = (double) depth * 0.1D + (double) ((x + z) & 0b1) * 0.2D;
                                 if(d3 < 0.5D) {
                                     shade = 255;
                                 } else if (d3 > 0.9D) {
@@ -150,21 +141,6 @@ public class ChunkView {
         }
     }
 
-    private void drawDebugImage() {
-        Graphics g = biomeMapImage.getGraphics();
-        try {
-            //Make it fancy
-            Graphics2D g2d = (Graphics2D) g;
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        }catch (Exception ignored) {}
-
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, biomeMapImage.getWidth(), biomeMapImage.getHeight());
-        g.setColor(Color.BLACK);
-        g.drawString(String.format("(%s,%s)", location.x, location.z), 1, 10);
-        g.dispose();
-    }
-
     public ChunkLocation getLocation() {
         return location;
     }
@@ -182,7 +158,7 @@ public class ChunkView {
     }
 
     public Rectangle getWorldBounds() {
-        return new Rectangle(location.x * Chunk.CHUNK_SIZE_X, location.z * Chunk.CHUNK_SIZE_Z, Chunk.CHUNK_SIZE_X, Chunk.CHUNK_SIZE_Z);
+        return bounds;
     }
 
     public void kill() {
